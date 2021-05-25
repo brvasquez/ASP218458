da3d1061c5aff9d6110f168c20ca3e1f6c395f10
brvasquez <bsvasquez43@misena.edu.co>
2021-05-24 22:31:05 -0500

    creacion controlador compras y crud

---
+++ ASP218458/Controllers/ComprasController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASP218458.Models;
+
+namespace ASP218458.Controllers
+{
+    public class ComprasController : Controller
+    {
+        private inventarioEntities db = new inventarioEntities();
+
+        // GET: Compras
+        public ActionResult Index()
+        {
+            var compra = db.compra.Include(c => c.cliente).Include(c => c.usuario);
+            return View(compra.ToList());
+        }
+
+        // GET: Compras/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            compra compra = db.compra.Find(id);
+            if (compra == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compra);
+        }
+
+        // GET: Compras/Create
+        public ActionResult Create()
+        {
+            ViewBag.id_cliente = new SelectList(db.cliente, "id", "nombre");
+            ViewBag.id_usuario = new SelectList(db.usuario, "id", "nombre");
+            return View();
+        }
+
+        // POST: Compras/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,fecha,total,id_usuario,id_cliente")] compra compra)
+        {
+            if (ModelState.IsValid)
+            {
+                db.compra.Add(compra);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.id_cliente = new SelectList(db.cliente, "id", "nombre", compra.id_cliente);
+            ViewBag.id_usuario = new SelectList(db.usuario, "id", "nombre", compra.id_usuario);
+            return View(compra);
+        }
+
+        // GET: Compras/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            compra compra = db.compra.Find(id);
+            if (compra == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.id_cliente = new SelectList(db.cliente, "id", "nombre", compra.id_cliente);
+            ViewBag.id_usuario = new SelectList(db.usuario, "id", "nombre", compra.id_usuario);
+            return View(compra);
+        }
+
+        // POST: Compras/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,fecha,total,id_usuario,id_cliente")] compra compra)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(compra).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.id_cliente = new SelectList(db.cliente, "id", "nombre", compra.id_cliente);
+            ViewBag.id_usuario = new SelectList(db.usuario, "id", "nombre", compra.id_usuario);
+            return View(compra);
+        }
+
+        // GET: Compras/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            compra compra = db.compra.Find(id);
+            if (compra == null)
+            {
+                return HttpNotFound();
+            }
+            return View(compra);
+        }
+
+        // POST: Compras/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            compra compra = db.compra.Find(id);
+            db.compra.Remove(compra);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}

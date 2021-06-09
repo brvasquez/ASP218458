570f5bdbacfe2a5c0de612f1b0bf00e1423978c7
brvasquez <bsvasquez43@misena.edu.co>
2021-06-09 17:34:18 -0500

    validations-vistas

--- ASP218458/Models/producto_compra.cs
+++ ASP218458/Models/producto_compra.cs
@@ -11,12 +11,16 @@ namespace ASP218458.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class producto_compra
     {
         public int id { get; set; }
+        [Required]
         public Nullable<int> id_compra { get; set; }
+        [Required]
         public Nullable<int> id_producto { get; set; }
+        [Required]
         public Nullable<int> cantidad { get; set; }
     
         public virtual compra compra { get; set; }

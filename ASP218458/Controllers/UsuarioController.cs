4ce49062cbe2d065a038a0042e2f1a60820db616
brvasquez <bsvasquez43@misena.edu.co>
2021-06-28 17:04:11 -0500

    paginador

--- ASP218458/Controllers/UsuarioController.cs
+++ ASP218458/Controllers/UsuarioController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using System.Web.Security;
 //importando los modelos de base de datos
 using ASP218458.Models;
+using System.Web.Routing;
 
 namespace ASP218458.Controllers
 {
@@ -175,6 +176,26 @@ namespace ASP218458.Controllers
         {
             FormsAuthentication.SignOut();
             return RedirectToAction("Index", "Home");
+        }
+
+        public ActionResult PaginadorIndex(int pagina = 1)
+        {
+            var cantidadRegistros = 5;
+            using (var db = new inventarioEntities())
+            {
+                var usuarios = db.usuario.OrderBy(x => x.id).Skip((pagina - 1) * cantidadRegistros)
+                    .Take(cantidadRegistros).ToList();
+
+                var totalRegistros = db.usuario.Count();
+                var modelo = new UsuarioIndex();
+                modelo.Usuarios = usuarios;
+                modelo.ActualPage = pagina;
+                modelo.Total = totalRegistros;
+                modelo.RecordsPage = cantidadRegistros;
+                modelo.ValuesQueryString = new RouteValueDictionary();
+
+                return View(modelo);
+            }
         }
 
     }
---
+++ ASP218458/Models/BaseModelo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Routing;
+
+namespace ASP218458.Models
+{
+    public class BaseModelo
+    {
+        public int ActualPage { get; set; }
+        public int Total { get; set; }
+        public int RecordsPage { get; set; }
+        public RouteValueDictionary ValuesQueryString { get; set; }
+    }
+}
